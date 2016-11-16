4395eca4e2213774de30ba085c36feb10ff7f2dd
Fran Lucena <franlucena27@gmail.com>
2016-11-16 11:25:25 +0100

    Updated local_config
    Changed password for connection in mongoDir

--- config/local_config.js
+++ config/local_config.js
@@ -9,7 +9,7 @@ module.exports = {
     platform: ['ios', 'android'],
     languages: ['en', 'es'],
     
-    mongoDir: 'mongodb://nodepopuser:danutoelputo1234@27017/nodepopdb',
+    mongoDir: 'mongodb://nodepopuser:gnS6EYsh4P4VySy3trCyYC59nmuzZYezsVqSXYGC@27017/nodepopdb',
     
     jwt: {
         secret: 'fspsefinds1235fdsfsacc14453tfdv34',
